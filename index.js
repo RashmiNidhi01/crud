5e07ad4a39aaff7e825f884ff4e2659abbd828d1
RashmiNidhi01 <rashmi.nidhi01@student.csulb.edu>
2021-04-26 14:26:23 -0500

    First release 1.0

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+
+// Creating the Express server
+const app = express();
+
+// Server configuration
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Starting the server
+app.listen(3008, () => {
+  console.log("Server started (http://localhost:3000/) !");
+});
+
+// GET /
+app.get("/", (req, res) => {
+  // res.send("Hello world...");
+  res.render("index");
+});
+
+// GET /about
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// GET /data
+app.get("/data", (req, res) => {
+  const test = {
+    title: "Test",
+    items: ["one", "two", "three"]
+  };
+  res.render("data", { model: test });
+});
